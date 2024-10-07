86e696c34469b89c5b8a3dfbe11cbae9cce5559a
Intissar <baloukintissar@gmail.com>
2024-10-07 12:13:10 +0200

    clase principal terminada

--- src/main/java/org/example/a/HelloApplication.java
+++ src/main/java/org/example/a/HelloApplication.java
@@ -2,22 +2,31 @@ package org.example.a;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
 public class HelloApplication extends Application {
+
     @Override
-    public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
-        stage.setScene(scene);
-        stage.show();
+    public void start(Stage primaryStage) {
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("hello-view.fxml"));
+            Parent root = fxmlLoader.load();
+            primaryStage.setScene(new Scene(root));
+            primaryStage.setTitle("Mi Aplicación JavaFX");
+            primaryStage.show();
+        } catch (IOException e) {
+            e.printStackTrace(); // Imprimir la traza de la excepción
+        }
     }
 
     public static void main(String[] args) {
-        launch();
+        launch(args);
     }
 }
